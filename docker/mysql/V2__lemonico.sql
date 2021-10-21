950f24f41c04c95c07c9dab91eba0bae05474f11
dev-resas <dev-resas@opentone.co.jp>
2021-10-21 22:29:35 +0900

    add docker

--- docker/mysql/V2__lemonico.sql
+++ docker/mysql/V2__lemonico.sql
@@ -7,6 +7,9 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='';
 
+DROP database IF EXISTS `lemonico`;
+CREATE DATABASE `lemonico`;
+
 -- -----------------------------------------------------
 -- Table `lemonico`.`User`
 -- -----------------------------------------------------
