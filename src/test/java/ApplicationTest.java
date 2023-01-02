f15682ba505c0f45bc57ce38ff66f6ecf03fad23
yuanmz9432 <yuanmz9432@gmail.com>
2023-01-02 16:18:17 +0900

    add junit test

---
+++ src/test/java/ApplicationTest.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2021 Blazeash Co.,Ltd. AllRights Reserved.
+ */
+
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.blazeash.api.ApplicationServer;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ApplicationServer.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ApplicationTest
+{
+    @Test
+    public void contextLoads() {}
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void testHeartbeat() throws Exception {
+        mockMvc.perform(get("/heartbeat")).andExpect(status().isNoContent());
+    }
+}
